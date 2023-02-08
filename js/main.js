b828352fb3d18b7ab7e1efc6511febd0d01e6d2b
Matias Laplace <matiaslaplace@hotmail.com>
2023-02-08 00:03:33 -0300

    Cambios para la segunda preentrega

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
 let opcion;
-let opcioncat;
-let carrito = 0;
+let opcionCat;
+
+const productos = [];
+
+function agregarItem(numero) {
+  productos[numero - 1].cantidadCarrito += 1;
+  productos[numero - 1].enCarrito = true;
+  productos[numero - 1].cantidad -= 1;
+}
+
+class Producto {
+  constructor(sku, nombre, precio, cantidad) {
+    this.sku = sku;
+    this.nombre = nombre;
+    this.precio = parseInt(precio);
+    this.cantidad = parseInt(cantidad);
+    this.enCarrito = false;
+    this.cantidadCarrito = 0;
+  }
+}
+
+productos.push(new Producto(1, "Llavero", 850, 4));
+productos.push(new Producto(2, "Remera", 2000, 2));
+productos.push(new Producto(3, "Gorra", 1250, 5));
+productos.push(new Producto(4, "Plancha de stickers", 350, 2));
+productos.push(new Producto(5, "Poster", 500, 8));
 
 opcion = parseInt(
   prompt(
@@ -11,50 +35,83 @@ opcion = parseInt(
 while (opcion != 3) {
   switch (opcion) {
     case 1:
-      opcioncat = parseInt(
+      opcionCat = parseInt(
         prompt(
-          "Ingrese el numero del item que le gustaria agregar al carrito. Aprete 6 para salir al menu principal y finalizar la compra\n1- Llavero | $850\n2- Remera | $2000\n3- Gorra | $1250\n4- Plancha de stickers | $350\n5- Poster | $500\n6- Volver al menu anterior"
+          `Ingrese el numero del item que le gustaria agregar al carrito. Aprete 6 para salir al menu principal y finalizar la compra\n${productos[0].sku} - ${productos[0].nombre} | ${productos[0].precio} \n${productos[1].sku} - ${productos[1].nombre} | ${productos[1].precio}\n${productos[2].sku} - ${productos[2].nombre} | ${productos[2].precio}\n${productos[3].sku} - ${productos[3].nombre} | ${productos[3].precio}\n${productos[4].sku} - ${productos[4].nombre} | ${productos[4].precio} \n6- Volver al menu anterior`
         )
       );
-      while (opcioncat != 6) {
-        switch (opcioncat) {
+
+      while (opcionCat != 6) {
+        switch (opcionCat) {
           case 1:
-            carrito = carrito + 850;
+            if (productos[opcionCat - 1].cantidad == 0) {
+              alert("La cantidad agregada es mayor al stock disponible");
+              break;
+            }
+            agregarItem(opcionCat);
             alert("Item agregado al carrito!");
             break;
+
           case 2:
-            carrito = carrito + 2000;
+            if (productos[opcionCat - 1].cantidad == 0) {
+              alert("La cantidad agregada es mayor al stock disponible");
+              break;
+            }
+            agregarItem(opcionCat);
             alert("Item agregado al carrito!");
             break;
+
           case 3:
-            carrito = carrito + 1250;
+            if (productos[opcionCat - 1].cantidad == 0) {
+              alert("La cantidad agregada es mayor al stock disponible");
+              break;
+            }
+            agregarItem(opcionCat);
             alert("Item agregado al carrito!");
             break;
+
           case 4:
-            carrito = carrito + 350;
+            if (productos[opcionCat - 1].cantidad == 0) {
+              alert("La cantidad agregada es mayor al stock disponible");
+              break;
+            }
+            agregarItem(opcionCat);
             alert("Item agregado al carrito!");
             break;
+
           case 5:
-            carrito = carrito + 500;
+            if (productos[opcionCat - 1].cantidad == 0) {
+              alert("La cantidad agregada es mayor al stock disponible");
+              break;
+            }
+            agregarItem(opcionCat);
             alert("Item agregado al carrito!");
             break;
+
           case 6:
             break;
           default:
             alert("Ingrese una opcion valida:");
         }
 
-        opcioncat = parseInt(
+        opcionCat = parseInt(
           prompt(
-            "Ingrese el numero del item que le gustaria agregar al carrito. Aprete 6 para salir al menu principal y finalizar la compra\n1- Llavero | $850\n2- Remera | $2000\n3- Gorra | $1250\n4- Plancha de stickers | $350\n5- Poster | $500\n6- Volver al menu anterior"
+            `Ingrese el numero del item que le gustaria agregar al carrito. Aprete 6 para salir al menu principal y finalizar la compra\n${productos[0].sku} - ${productos[0].nombre} | ${productos[0].precio} \n${productos[1].sku} - ${productos[1].nombre} | ${productos[1].precio}\n${productos[2].sku} - ${productos[2].nombre} | ${productos[2].precio}\n${productos[3].sku} - ${productos[3].nombre} | ${productos[3].precio}\n${productos[4].sku} - ${productos[4].nombre} | ${productos[4].precio} \n6- Volver al menu anterior`
           )
         );
       }
+      // console.log(productos);
       break;
 
     case 2:
-      alert("El total de su compra es: $" + carrito);
-      carrito = 0;
+      const carrito = productos.filter((el) => el.enCarrito == true);
+      let avisoCarrito = "Su carrito contiene: \n\n";
+      for (const producto of carrito) {
+        avisoCarrito =
+          avisoCarrito +
+          `${producto.nombre} | Cantidad: ${producto.cantidadCarrito}\n`;
+      }
+      alert(avisoCarrito);
       break;
 
     case 3:
