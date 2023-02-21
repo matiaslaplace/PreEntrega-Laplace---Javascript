cb90d0162913e409f7f51774ca7e6d4351c2fedb
Matias Laplace <matiaslaplace@hotmail.com>
2023-02-21 19:58:46 -0300

    Actualizacion 3er preentrega

--- js/main.js
+++ js/main.js
@@ -1,22 +1,89 @@
-let opcion;
-let opcionCat;
-let opcionCarrito;
-let opcionEliminar;
-
 const productos = [];
+let subtotal = 0; //para calcular el subtotal de la compra
+let totalItems = 0; //para calcular la cantidad de items en el carrito
+const guardarLocal = (clave, valor) => {
+  localStorage.setItem(clave, valor);
+};
+
+function textoSubtotal() {
+  totalItems = 0;
+  for (const producto of productos) {
+    totalItems = totalItems + producto.cantidadCarrito;
+  }
+
+  let subtotal1 = document.getElementById("subtotal");
+  subtotal1.innerText = `$${subtotal}`;
+  let textoamount = document.getElementById("items");
+  textoamount.innerText = `${totalItems} items`;
+}
 
 function agregarItem(numero) {
-  productos[numero - 1].cantidadCarrito += 1;
-  productos[numero - 1].enCarrito = true;
-  productos[numero - 1].cantidad -= 1;
+  if (productos[numero - 1].cantidad == 0) {
+    document.getElementById(`maxStock${numero}`).style.visibility = "visible";
+  } else {
+    let texto = document.getElementById(`contador${numero}`);
+    let amount = document.getElementById(`amount${numero}`);
+
+    document.getElementById(`remove${numero}`).style.visibility = "visible";
+
+    productos[numero - 1].cantidadCarrito += 1;
+    productos[numero - 1].enCarrito = true;
+    productos[numero - 1].cantidad -= 1;
+
+    amount.innerText = `$${
+      productos[numero - 1].precio * productos[numero - 1].cantidadCarrito
+    }`;
+    subtotal = subtotal + productos[numero - 1].precio;
+    texto.innerText = productos[numero - 1].cantidadCarrito;
+
+    textoSubtotal();
+
+    guardarLocal("Carrito", JSON.stringify(productos));
+  }
 }
 
 function eliminarItem(numero) {
-  productos[numero - 1].cantidadCarrito -= 1;
-  if (productos[numero - 1].cantidadCarrito == 0) {
-    productos[numero - 1].enCarrito = false;
+  if (productos[numero - 1].cantidadCarrito > 0) {
+    document.getElementById(`maxStock${numero}`).style.visibility = "hidden";
+    let texto = document.getElementById(`contador${numero}`);
+    let amount = document.getElementById(`amount${numero}`);
+    productos[numero - 1].cantidadCarrito -= 1;
+    if (productos[numero - 1].cantidadCarrito == 0) {
+      productos[numero - 1].enCarrito = false;
+      document.getElementById(`remove${numero}`).style.visibility = "hidden";
+    }
+    productos[numero - 1].cantidad += 1;
+    amount.innerText = `$${
+      productos[numero - 1].precio * productos[numero - 1].cantidadCarrito
+    }`;
+    subtotal = subtotal - productos[numero - 1].precio;
+    texto.innerText = productos[numero - 1].cantidadCarrito;
+
+    textoSubtotal();
+    guardarLocal("Carrito", JSON.stringify(productos));
   }
-  productos[numero - 1].cantidad += 1;
+}
+
+function removeAll() {
+  for (let i = 1; i < 6; i++) {
+    while (productos[i - 1].cantidadCarrito > 0) {
+      eliminarItem(i);
+    }
+  }
+}
+
+function renderizarCarrito() {
+  for (let i = 1; i < 6; i++) {
+    while (productos[i - 1].cantidadCarrito < carrito[i - 1].cantidadCarrito) {
+      agregarItem(i);
+    }
+  }
+}
+
+function remove(numero) {
+  while (productos[numero - 1].cantidadCarrito > 0) {
+    eliminarItem(numero);
+  }
 }
 
 class Producto {
@@ -30,112 +97,69 @@ class Producto {
   }
 }
 
+document.getElementById("remove1").style.visibility = "hidden";
+document.getElementById("remove2").style.visibility = "hidden";
+document.getElementById("remove3").style.visibility = "hidden";
+document.getElementById("remove4").style.visibility = "hidden";
+document.getElementById("remove5").style.visibility = "hidden";
+document.getElementById("maxStock1").style.visibility = "hidden";
+document.getElementById("maxStock2").style.visibility = "hidden";
+document.getElementById("maxStock3").style.visibility = "hidden";
+document.getElementById("maxStock4").style.visibility = "hidden";
+document.getElementById("maxStock5").style.visibility = "hidden";
+
 productos.push(new Producto(1, "Llavero", 850, 4));
 productos.push(new Producto(2, "Remera", 2000, 2));
 productos.push(new Producto(3, "Gorra", 1250, 5));
 productos.push(new Producto(4, "Plancha de stickers", 350, 2));
 productos.push(new Producto(5, "Poster", 500, 8));
 
-opcion = parseInt(
-  prompt(
-    "Ingrese el numero de la opcion que le gustaria elegir: \n1- Mostrar catalogo\n2- Finalizar compra\n3- Salir"
-  )
-);
-
-while (opcion != 3) {
-  switch (opcion) {
-    case 1:
-      opcionCat = parseInt(
-        prompt(
-          `Ingrese el numero del item que le gustaria agregar al carrito. Aprete 6 para salir al menu principal y finalizar la compra\n${productos[0].sku} - ${productos[0].nombre} | ${productos[0].precio} \n${productos[1].sku} - ${productos[1].nombre} | ${productos[1].precio}\n${productos[2].sku} - ${productos[2].nombre} | ${productos[2].precio}\n${productos[3].sku} - ${productos[3].nombre} | ${productos[3].precio}\n${productos[4].sku} - ${productos[4].nombre} | ${productos[4].precio} \n6- Volver al menu anterior`
-        )
-      );
-
-      while (opcionCat != 6) {
-        switch (opcionCat) {
-          case 1:
-          case 2:
-          case 3:
-          case 4:
-          case 5:
-            if (productos[opcionCat - 1].cantidad == 0) {
-              alert("La cantidad agregada es mayor al stock disponible");
-              break;
-            }
-            agregarItem(opcionCat);
-            alert("Item agregado al carrito!");
-            break;
-
-          case 6:
-            break;
-          default:
-            alert("Ingrese una opcion valida:");
-        }
-
-        opcionCat = parseInt(
-          prompt(
-            `Ingrese el numero del item que le gustaria agregar al carrito. Aprete 6 para salir al menu principal y finalizar la compra\n${productos[0].sku} - ${productos[0].nombre} | ${productos[0].precio} \n${productos[1].sku} - ${productos[1].nombre} | ${productos[1].precio}\n${productos[2].sku} - ${productos[2].nombre} | ${productos[2].precio}\n${productos[3].sku} - ${productos[3].nombre} | ${productos[3].precio}\n${productos[4].sku} - ${productos[4].nombre} | ${productos[4].precio} \n6- Volver al menu anterior`
-          )
-        );
-      }
-
-      break;
-
-    case 2:
-      do {
-        let carrito = productos.filter((el) => el.enCarrito == true);
-        let avisoCarrito = "Su carrito contiene: \n\n";
-        let avisoEliminarCarrito =
-          "Ingrese el numero del item que desea eliminar: \n\n";
-        let total = 0;
-        for (const producto of carrito) {
-          avisoCarrito =
-            avisoCarrito +
-            `${producto.nombre}   |   Cantidad: ${producto.cantidadCarrito}   |   Precio: $${producto.precio}\n`;
-          avisoEliminarCarrito =
-            avisoEliminarCarrito +
-            `${producto.sku}. ${producto.nombre}   |   Cantidad: ${producto.cantidadCarrito}\n`;
-          total = total + producto.cantidadCarrito * producto.precio;
-        }
-        avisoCarrito =
-          avisoCarrito +
-          `\nTotal: $${total}\n\nIngrese 1 para finalizar la compra o 2 para eliminar algun item del carrito:`;
-
-        opcionCarrito = parseInt(prompt(avisoCarrito));
-
-        switch (opcionCarrito) {
-          case 1:
-            alert(
-              `Su Total es: $${total}\n\nMuchas gracias por comprar con nosotros!`
-            );
-            for (const producto of carrito) {
-              producto.enCarrito = false;
-              producto.cantidadCarrito = 0;
-            }
-            break;
-
-          case 2:
-            opcionEliminar = parseInt(prompt(avisoEliminarCarrito));
-            eliminarItem(opcionEliminar);
-            break;
-
-          default:
-            alert("Ingrese una opcion valida:");
-        }
-      } while (opcionCarrito != 1);
-      break;
-
-    case 3:
-      break;
-
-    default:
-      alert("Elija una opcion valida");
-      break;
-  }
-
-  opcion = parseInt(
-    prompt(
-      "Ingrese el numero de la opcion que le gustaria elegir: \n1- Mostrar catalogo\n2- Finalizar compra\n3- Salir"
-    )
-  );
+let carrito = [];
+let carritoEnJS = localStorage.getItem("Carrito");
+if (carritoEnJS != null) {
+  carrito = JSON.parse(carritoEnJS);
+  renderizarCarrito();
 }
+
+let boton11 = document.getElementById("btnSumar1");
+boton11.addEventListener("click", () => agregarItem(1));
+let boton12 = document.getElementById("btnRestar1");
+boton12.addEventListener("click", () => eliminarItem(1));
+
+let boton21 = document.getElementById("btnSumar2");
+boton21.addEventListener("click", () => agregarItem(2));
+let boton22 = document.getElementById("btnRestar2");
+boton22.addEventListener("click", () => eliminarItem(2));
+
+let boton31 = document.getElementById("btnSumar3");
+boton31.addEventListener("click", () => agregarItem(3));
+let boton32 = document.getElementById("btnRestar3");
+boton32.addEventListener("click", () => eliminarItem(3));
+
+let boton41 = document.getElementById("btnSumar4");
+boton41.addEventListener("click", () => agregarItem(4));
+let boton42 = document.getElementById("btnRestar4");
+boton42.addEventListener("click", () => eliminarItem(4));
+
+let boton51 = document.getElementById("btnSumar5");
+boton51.addEventListener("click", () => agregarItem(5));
+let boton52 = document.getElementById("btnRestar5");
+boton52.addEventListener("click", () => eliminarItem(5));
+
+let botonRemoveAll = document.getElementById("removeAll");
+botonRemoveAll.addEventListener("click", removeAll);
+
+let botonRemove1 = document.getElementById("remove1");
+botonRemove1.addEventListener("click", () => remove(1));
+
+let botonRemove2 = document.getElementById("remove2");
+botonRemove2.addEventListener("click", () => remove(2));
+
+let botonRemove3 = document.getElementById("remove3");
+botonRemove3.addEventListener("click", () => remove(3));
+
+let botonRemove4 = document.getElementById("remove4");
+botonRemove4.addEventListener("click", () => remove(4));
+
+let botonRemove5 = document.getElementById("remove5");
+botonRemove5.addEventListener("click", () => remove(5));
