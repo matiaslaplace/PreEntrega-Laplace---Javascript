f6c854355d769a4916f4360de7afe5b6aae2fd9d
Matias Laplace <matiaslaplace@hotmail.com>
2023-03-18 12:16:55 -0300

    prueba Pages

--- js/main.js
+++ js/main.js
@@ -103,7 +103,7 @@ function modificarIconoCarrito() {
 }
 
 function obtenerProductosDelJSON() {
-  fetch("../productos.json")
+  fetch("/productos.json")
     .then((response) => {
       return response.json();
     })
